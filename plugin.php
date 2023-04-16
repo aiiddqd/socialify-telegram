e60d3349e4e4efa013e781af2c8e4221ccb290ee
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2023-04-16 18:54:57 +0300

    init

---
+++ plugin.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * Plugin Name:  Socialify Telegram
+ * Description:  Login via Telegram
+ * Plugin URI:   https://github.com/uptimizt/socialify
+ * Author:       uptimizt
+ * Author URI:   https://github.com/uptimizt
+ * Text Domain:  socialify
+ * Domain Path:  /languages/
+ * GitHub Plugin URI: https://github.com/uptimizt/socialify
+ * Requires PHP: 8.0
+ * Version:      0.9.230416
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+*/
+
+namespace Socialify\Telegram;
+
+use Socialify\ProviderAbstract;
+// final class Telegram extends ProviderAbstract {
+
+    
+// }
